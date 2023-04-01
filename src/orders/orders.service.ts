a5a8cf9185bca9b3f88713b991db30e28d00c5ce
Konrad Brożyna <kondzio3002@gmail.com>
2023-04-01 21:05:08 +0200

    Add endpoints for orders

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Order, db } from 'src/db';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  public getAll(): Order[] {
+    return db.orders;
+  }
+
+  public getById(id: Order['id']): Order | null {
+    return db.orders.find(o => o.id === id);
+  }
+}
